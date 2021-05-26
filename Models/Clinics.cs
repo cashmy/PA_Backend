6bee4444faeef0de705a19fdc59836d4ae99f3e7
cashmy <cmyers880@gmail.com>
2021-05-26 09:32:21 -0500

    Add Clinic Master

---
+++ Models/Clinics.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PA_Backend.Models
+{
+    public class Clinic
+    {
+        public int ClinicId { get; set; }
+        public string ClinicName { get; set; }
+        public string ClinicAddress1 { get; set; }
+        public string ClinicAddress2 { get; set; }
+        public string ClinicCity { get; set; }
+        public string ClinicState { get; set; }
+        public string ClinicZip { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string ClinicPhone { get; set; }
+        public long ClinicNPI { get; set; }
+        public bool ClinicIsAGroup { get; set; }
+
+    }
+}
