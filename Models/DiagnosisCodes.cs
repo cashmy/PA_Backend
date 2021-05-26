f315df59a35105ff3e888a3eb3bff05cf583b928
cashmy <cmyers880@gmail.com>
2021-05-25 23:11:14 -0500

    Added DiagnosisCodes Master File

---
+++ Models/DiagnosisCodes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace PA_Backend.Models
+{
+    public class DiagnosisCode
+    {
+        [Key]
+        public string DiagCode { get; set; }
+        public string DiagDescription { get; set; }
+    }
+}
