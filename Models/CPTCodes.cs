5b2fe41514847f8bb0995caa251fff08d71c5373
cashmy <cmyers880@gmail.com>
2021-05-25 23:10:55 -0500

    Added CPTCodes Master File

---
+++ Models/CPTCodes.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PA_Backend.Models
+{
+    public class CPTCode
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CPTCodeId { get; set; }
+        public string CPTDescription { get; set; }
+    }
+}
