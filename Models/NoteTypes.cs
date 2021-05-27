099dc3506e4a6640c7e10b2f812feb9e92f0db1a
cashmy <cmyers880@gmail.com>
2021-05-27 06:59:06 -0500

    Added NoteType Master

---
+++ Models/NoteTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PA_Backend.Models
+{
+    public class NoteType
+    {
+        public int NoteTypeId { get; set; }
+        public string NoteTypeName { get; set; }
+    }
+}
