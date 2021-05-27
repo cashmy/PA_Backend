fa1f3126f15a0cb596e4e0fd3ed1281f664f8bd6
cashmy <cmyers880@gmail.com>
2021-05-27 06:58:14 -0500

    Added Patient and Provider models

---
+++ Models/Patients.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PA_Backend.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string PatientFirstName { get; set; }
+        public string PatientLastName { get; set; }
+        public DateTime PatientDOB { get; set; }
+        public bool PatientHaveIEP { get; set; }
+        public bool PatientInABA { get; set; }
+        public string PatientClass { get; set; }
+        [StringLength(256)]
+        public string PatientNotes { get; set; }
+        public bool PatientInactive { get; set; }
+    }
+}
---
+++ Models/Providers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA_Backend.Models
+{
+    public class Provider
+    {
+        public int ProviderId { get; set; }
+
+        // Provider's UserID is an optional field
+        // Manually validated against the role of "Provider"
+        public string ProviderUserId { get; set; }
+
+        public string ProviderFirstName { get; set; }
+        public string ProviderLastName { get; set; }
+        
+        // Default to user email unless "ProviderUserId is blank than this field should be required 
+        public string ProviderEmail { get; set; }
+        public bool ProviderRcvEmails { get; set; }
+        public long ProviderNPI { get; set; }
+        public string ProviderTaxonomy { get; set; }
+
+        //Validated against the role of "staff"
+        [ForeignKey("User")]
+        public string AssignedStaffUserId { get; set; }
+        public User User { get; set; }
+
+        [StringLength(256)]
+        public string ProviderNotes { get; set; }
+    }
+}
