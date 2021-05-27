dd37b6210a6f504868ca73d555e6187471cebf2f
cashmy <cmyers880@gmail.com>
2021-05-27 07:06:29 -0500

    Added POS and Treatment class Endpoints

---
+++ Controllers/PlaceOfServiceController.cs
@@ -0,0 +1,83 @@
+﻿using PA_Backend.Data;
+using PA_Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+
+namespace PA_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaceOfServiceController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public PlaceOfServiceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // ***** GET ALL PlacesOfService *****
+        // <baseurl>/api/placeofservice
+        [HttpGet, Authorize]
+        public IActionResult Get()
+        {
+            var placesOfService = _context.PlacesOfServices.ToList();
+            if (placesOfService == null)
+            {
+                return NotFound();
+            }
+            return Ok(placesOfService);
+        }
+        // ***** GET A PlaceOfService by ID *****
+        // <baseurl>/api/placeofservice
+        [HttpGet("{code}"), Authorize]
+        public IActionResult GetById(string code)
+        {
+            var placeOfService = _context.PlacesOfServices.Where(p => p.PlaceOfServiceCode == code).SingleOrDefault();
+            return Ok(placeOfService);
+        }
+        // ***** ADD A PlaceOfService *****
+        // POST /api/placeofservice
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody] PlaceOfService value)
+        {
+            _context.PlacesOfServices.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+
+        // ***** UPDATE A PlaceOfService *****
+        // PUT /api/placeofservice
+        [HttpPut("{code}"), Authorize]
+        public IActionResult Put(string code, [FromBody] PlaceOfService value)
+        {
+            var placeOfService = _context.PlacesOfServices.Where(p => p.PlaceOfServiceCode == code).SingleOrDefault();
+            if (placeOfService == null)
+            {
+                return NotFound("Requested record not found.");
+            }
+            placeOfService.PlaceOfServiceDesc = value.PlaceOfServiceDesc;
+
+            _context.PlacesOfServices.Update(placeOfService);
+            _context.SaveChanges();
+            return StatusCode(201, placeOfService);
+        }
+
+
+        // ***** DELETE A PlaceOfService *****
+        // DELETE /api/placeofservice
+        [HttpDelete("{code}"), Authorize]
+        public IActionResult Delete(string code)
+        {
+
+            var placeOfService = _context.PlacesOfServices.Where(p => p.PlaceOfServiceCode == code).SingleOrDefault();
+            if (placeOfService == null)
+            {
+                return NotFound("Requested record not found.");
+            }
+
+            _context.PlacesOfServices.Remove(placeOfService);
+            _context.SaveChanges();
+            return StatusCode(204, placeOfService);
+        }
+    }
+}
---
+++ Controllers/TreatmentClassController.cs
@@ -0,0 +1,84 @@
+﻿using PA_Backend.Data;
+using PA_Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+
+
+namespace PA_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreatmentClassController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public TreatmentClassController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // ***** GET ALL Treatments-Therapies *****
+        // <baseurl>/api/treatmentClass
+        [HttpGet, Authorize]
+        public IActionResult Get()
+        {
+            var treatments = _context.TreatmentClass.ToList();
+            if (treatments == null)
+            {
+                return NotFound();
+            }
+            return Ok(treatments);
+        }
+        // ***** GET A Treatment-Therapy by ID *****
+        // <baseurl>/api/treatmentClass
+        [HttpGet("{code}"), Authorize]
+        public IActionResult GetById(string code)
+        {
+            var treatment = _context.TreatmentClass.Where(p => p.TreatmentCode == code).SingleOrDefault();
+            return Ok(treatment);
+        }
+        // ***** ADD A Treatment-Therapy *****
+        // POST /api/treatmentClass
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody] Treatment value)
+        {
+            _context.TreatmentClass.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+
+        // ***** UPDATE A Treatment-Therapy *****
+        // PUT /api/treatmentClass
+        [HttpPut("{code}"), Authorize]
+        public IActionResult Put(string code, [FromBody] Treatment value)
+        {
+            var treatment = _context.TreatmentClass.Where(p => p.TreatmentCode == code).SingleOrDefault();
+            if (treatment == null)
+            {
+                return NotFound("Requested record not found.");
+            }
+            treatment.TreatmentName = value.TreatmentName;
+
+            _context.TreatmentClass.Update(treatment);
+            _context.SaveChanges();
+            return StatusCode(201, treatment);
+        }
+
+
+        // ***** DELETE A Treatment-Therapy *****
+        // DELETE /api/treatmentClass
+        [HttpDelete("{code}"), Authorize]
+        public IActionResult Delete(string code)
+        {
+
+            var treatment = _context.TreatmentClass.Where(p => p.TreatmentCode == code).SingleOrDefault();
+            if (treatment == null)
+            {
+                return NotFound("Requested record not found.");
+            }
+
+            _context.TreatmentClass.Remove(treatment);
+            _context.SaveChanges();
+            return StatusCode(204, treatment);
+        }
+    }
+}
